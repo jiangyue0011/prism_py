4af7597a892d8d1da451c405ed5d255beed3fa39
jiangyue <jiangyue@s4-8.bioracks>
2018-12-19 14:35:57 +0800

    add some comment

--- prism_hash_numpy.py
+++ prism_hash_numpy.py
@@ -45,7 +45,7 @@ class Reference(object):
 
 def hash(db, query, kmer_len, matrix1_width):
     """
-    
+    Hash db kmers to quickly find matched kmers between query and db.
     """
     kmer_dict = {}
     matched_kmers = []
@@ -55,18 +55,10 @@ def hash(db, query, kmer_len, matrix1_width):
     for i in xrange(matrix1_width, len(db)-kmer_len+1):  # hash kmer in ref2
         kmer_dict.setdefault(db[i:i+kmer_len], []).append(i)
         print "ref2 kmer", i, db[i:i+kmer_len]
-    '''
-    for i in xrange(0, len(ref1)-kmer_len):
-        kmer_dict.setdefault(ref1[i:i+kmer_len], []).append(i)
-    for i in xrange(0, len(ref2)-kmer_len):
-        kmer_dict.setdefault(ref2[i:i+kmer_len], []).append(i+len(ref1))
-    '''
-    for i in xrange(1, len(query)-kmer_len+1):
+    for i in xrange(1, len(query)-kmer_len+1):  # look for query kmers in db kmer dict
         q_kmer = query[i:i+kmer_len]
         print "read kmer", i, q_kmer
         if q_kmer in kmer_dict:  # look for query kmer in hasded db
-            #for j in sorted(kmer_dict[kmer]):
-            #for j in kmer_dict[q_kmer]:
             if len(kmer_dict[q_kmer]) == 1:  # only use unique kmers
                 j = kmer_dict[q_kmer][0]
                 matched_kmers.append([i, j, 0])  # [query_pos, db_pos, used_for_merge]
@@ -122,10 +114,10 @@ def cal_sw_score(sw_matrix, row_idx, col_idx, match, jump_end, matrix1_width):
         cell_up = sw_matrix[row_idx-1][col_idx]
         cell_diag = sw_matrix[row_idx-1][col_idx-1]
         cell_left = sw_matrix[row_idx][col_idx-1]
-        cell['score_up_diag'] = cell_up['score_diag'] + score_gap_open
+        cell['score_up_diag'] = cell_up['score_diag'] + score_gap_open + score_gap_ext
         cell['score_up_up'] = max(cell_up['score_up_up'], cell_up['score_up_diag']) + score_gap_ext
         cell['score_diag'] = cell_diag['score_max'] + score_match_mismatch
-        cell['score_left_diag'] = cell_left['score_diag'] + score_gap_open
+        cell['score_left_diag'] = cell_left['score_diag'] + score_gap_open + score_gap_ext
         cell['score_left_left'] = max(cell_left['score_left_left'], cell_left['score_left_diag']) + score_gap_ext
     
     jump_idx = 0
@@ -136,35 +128,13 @@ def cal_sw_score(sw_matrix, row_idx, col_idx, match, jump_end, matrix1_width):
         print "jump end:", (row_idx, col_idx), jump_end, jump_idx, cell['score_jump']
         if sw_matrix[row_idx-1][jump_idx]['filled'] == 1:
             cell['score_jump'] += score_jump + score_match_mismatch
-            cell['source_col'] = jump_idx
-        else:
-            cell['source_col'] = -1
+            cell['jump_col'] = jump_idx
             #cell['score_jump'] = -999
     #print "cell", row_idx, col_idx, cell
     score_list = [cell['score_diag'], cell['score_up_up'], cell['score_up_diag'], cell['score_left_left'], cell['score_left_diag'], cell['score_jump']]  # "jump" is the last choice
     print "cell info", row_idx, col_idx, score_list
     cell['score_max'] = max(score_list)
     cell['filled'] = 1
-    '''
-    score_list = [cell['score_diag'], cell['score_jump'], cell['score_up'], cell['score_left']]
-    cell['score_max'] = max(score_list)
-    max_idx = score_list.index(cell['score_max'])
-    if max_idx == 0: # max score from diag
-        cell['source_row'] = row_idx-1
-        cell['source_col'] = col_idx-1
-    elif max_idx == 1: # max score from jump
-        cell['source_row'] = row_idx-1 
-        cell['source_col'] = jump_idx
-    elif max_idx == 2: # max score from up
-        cell['source_row'] = row_idx-1
-        cell['source_col'] = col_idx
-    else: # max score from left
-        cell['source_row'] = row_idx
-        cell['source_col'] = col_idx-1
-    cell['filled'] = 1
-    #print "cell", row_idx, col_idx, cell
-    #print cell.score_up, cell.score_diag, cell.score_left, cell.score_jump, cell.score_max
-    '''
 
 def fill_sw_matrix(sw_matrix, db, query, row_start, row_end, col_start, col_end, matrix1_width, ref_overlap, fill_type="RECTANGLE"):
     for i in range(row_start, row_end+1):
@@ -187,27 +157,16 @@ def sw_split(ref_seq1, ref_seq2, read_seq, ref_overlap):
     matrix1_width = len(ref_seq1) + 1
     n_rows = len(query)
     n_cols = len(db)
-    swcell = np.dtype({'names':['score_up_diag', 'score_up_up', 'score_left_diag', 'score_left_left', 'score_diag', 'score_jump', 'score_max', 'source_row', 'source_col', 'filled'], 
-                       'formats':['i', 'i', 'i', 'i', 'i', 'i', 'i', 'i', 'i', 'i']}, align = True)
-    #sw_matrix = np.zeros(shape=(n_rows, n_cols), dtype=swcell)
+    swcell = np.dtype({'names':['score_up_diag', 'score_up_up', 'score_left_diag', 'score_left_left', 'score_diag', 'score_jump', 'score_max', 'jump_col', 'filled'], 
+                       'formats':['i', 'i', 'i', 'i', 'i', 'i', 'i', 'i', 'i']}, align = True)
     sw_matrix = np.full((n_rows, n_cols), -999, dtype=swcell)
-    #sw_matrix = [[SWCell() for col in range(n_cols)] for row in range(n_rows)]
-    #sw_matrix = [[SWCell() for col in range(n_cols)] for row in range(n_rows)]
-    #sw_matrix = np.array([[SWCell() for col in range(n_cols)] for row in range(n_rows)], dtype=object)
-    #sw_matrix = np.full((n_rows, n_cols), SWCell())
-    #sw_matrix = np.ndarray(shape=(n_rows, n_cols), dtype=SWCell())
-    #print "nrow, ncol, mwidth:", n_rows, n_cols, matrix1_width, sw_matrix.shape
     
     # initialize first row and first column
     for i in xrange(0, n_rows):
         sw_matrix[i][0]['score_max'] = -(40 + i*7)
-        sw_matrix[i][0]['source_row'] = i - 1
-        sw_matrix[i][0]['source_col'] = 0
     for j in xrange(0, n_cols):
         sw_matrix[0][j]['score_max'] = 0
 
-    for i in xrange(1, n_rows):
-        sw_matrix[i][0]['source_row'] = i-1
     kmer_list = []
     kmer_list = hash(db, query, 3, matrix1_width)
     row_start = 1
@@ -253,11 +212,19 @@ def sw_split(ref_seq1, ref_seq2, read_seq, ref_overlap):
     #    for j in range(n_cols):
     #        print sw_matrix[i][j].score_max,
     #    print
-    align_start, cigar = backtrace(sw_matrix)
+    align_start, cigar, score = backtrace(sw_matrix)
     print_alignment(db, query, align_start, cigar)
+
+    return align_start, cigar, score
 
 
 def backtrace(sw_matrix):
+    """
+    Backtrace of the sw-matirx. It starts from the first max score cell of the last row. When the max score
+    if from diag cell, the score can be any of the diag cell's scores. When the max score is score_up_diag,
+    it means this score is from up cell, and the score of up cell is from up cell's diag cell. In this case,
+    the next two steps are fixed. The path has to go up then go diag.  
+    """
     row_idx = len(sw_matrix)-1
     last_row_scores = [sw_matrix[row_idx][j]['score_max'] for j in xrange(0, len(sw_matrix[0]))]
     max_score = max(last_row_scores)
@@ -269,42 +236,39 @@ def backtrace(sw_matrix):
     code_len = 0
     code_list = []
     next_cell = ""  # to force next cell go according to left_left left_diag up_up up_diag
+    jump_len = 0
+
     while row_idx > 0:
         if row_idx == 1:
             align_pos = col_idx
         print "backtrace", (row_idx, col_idx), next_cell
-        is_jump = False
         cell = sw_matrix[row_idx][col_idx]
         if not next_cell:
-            #if row_idx == cell['source_row'] + 1 and col_idx == cell['source_col'] + 1: # match/mismatch
-            if cell['score_max'] == cell['score_diag']:
+            if cell['score_max'] == cell['score_diag']:  # match/mismatch
                 row_idx -= 1
                 col_idx -= 1
                 cur_code = "M"
-            #elif row_idx == cell['source_row'] and col_idx == cell['source_col'] + 1: # deletion
-            elif cell['score_max'] == cell['score_left_left']:
+            elif cell['score_max'] == cell['score_left_left']:  # deletion
                 col_idx -= 1
                 cur_code = "D"
                 next_cell = "left"
-            #elif row_idx == cell['source_row'] + 1 and col_idx == cell['source_col']: # insertion
-            elif cell['score_max'] == cell['score_left_diag']:
+            elif cell['score_max'] == cell['score_left_diag']:  # deletion
                 col_idx -= 1
                 cur_code = "D"
                 next_cell = "diag"
-            elif cell['score_max'] == cell['score_up_up']:
+            elif cell['score_max'] == cell['score_up_up']:  # insertion
                 row_idx -= 1
                 cur_code = "I"
                 next_cell = "up"
-            elif cell['score_max'] == cell['score_up_diag']:
+            elif cell['score_max'] == cell['score_up_diag']:  # insertion
                 row_idx -= 1
                 cur_code = "I"
                 next_cell = "diag"
-            else: # jump
+            else:  # jump
                 cur_code = "M"
-                is_jump = True
-                jump_len = col_idx - cell['source_col'] - 1
+                jump_len = col_idx - cell['jump_col'] - 1
                 row_idx -= 1
-                col_idx = cell['source_col']
+                col_idx = cell['jump_col']
                 next_cell = "diag"
         elif next_cell == "diag":
             row_idx -= 1
@@ -321,34 +285,31 @@ def backtrace(sw_matrix):
             cur_code = "I"
             if cell['score_max'] == cell['score_up_diag']:
                 next_cell = "diag"
-
-        print "is jump", cur_code, is_jump
-
+    
+        # build CIGAR string
         if cur_code != last_code and last_code != "":
             code_list.append(str(code_len)+last_code)
             code_len = 1
         else:
             code_len += 1
-
-        if is_jump:
-            if jump_len: # for the case that ref1 and ref2 are continuous
-                code_list.append(str(code_len)+cur_code)
-                cur_code = "N"
-                code_len = jump_len
-            #code_list.append(str(code_len)+cur_code)
+        if jump_len: # for the case that ref1 and ref2 are continuous
+            code_list.append(str(code_len)+cur_code)
+            cur_code = "N"
+            code_len = jump_len
+            jump_len = 0
         last_code = cur_code
-        #row_idx = cell['source_row']
-        #col_idx = cell['source_col']
     code_list.append(str(code_len)+cur_code)
-    print code_list
     cigar = "".join(code_list[::-1])
-    print "CIGAR:", cigar
     if cur_code != "M":
         utils.message("ERROR: alignment starts with indels." + cur_code, exit=1)
-    return align_pos, cigar
+
+    return align_pos, cigar, max_score
 
 
 def print_sw_matrix(sw_matrix):
+    """
+    Print socre_max for each cell in the format of a matrix
+    """
     for i in xrange(0, sw_matrix.shape[0]):
         for j in xrange(0, sw_matrix.shape[1]):
             print str(sw_matrix[i][j]['score_max']).rjust(4),
@@ -356,6 +317,9 @@ def print_sw_matrix(sw_matrix):
 
 
 def print_alignment(db, query, pos, cigar):
+    """
+    Print alignment of db and query
+    """
     print db, query, pos, cigar
     out_query = ""
     out_db = ""
@@ -408,8 +372,8 @@ if __name__ == '__main__':
     print ref.ref_dict
 
     #sw_split(ref_seq1, ref_seq2, read_seq)
-    ref1 = "BACCABBB"
-    ref2 = "BBBDACDFFCAB"
+    ref1 = "BACCABBB" * 2
+    ref2 = "BBBDACDFFCAB" * 2
     read = "ACCADACDEEFFCA"
     #ref1 = "BBBBAAAA"
     #ref2 = "BBBBAAAA"
